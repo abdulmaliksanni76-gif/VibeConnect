70c9e1418621fc4d9964dce2008a48f233c2e796
abdulmaliksanni76-gif <abdulmaliksanni76@gmail.com>
2026-07-10 22:18:27 +0100

    vibeconnect

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,19 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const messageSchema = new mongoose.Schema({
-//   conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
-//   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   text: String,
-//   fileUrl: String,   // New field
-//   fileType: String,  // e.g., 'image', 'pdf'
-//   status: { type: String, default: 'sent' }
-// }, { timestamps: true });
-
-// // module.exports = mongoose.model('Message', messageSchema);
-// const Message = mongoose.model('Message', messageSchema);
-// export default Message;
-
-
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
